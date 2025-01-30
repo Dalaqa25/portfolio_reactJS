ea744ed0f6d28a2132ca4a4146a2a7464617b568
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-01-30 18:01:01 +0400

    old cake

--- components/body.jsx
+++ components/body.jsx
@@ -1,16 +1,31 @@
 import '../assets/body.css';
+import profileimg from '../assets/profileimg.png';
+import  {useState} from 'react'; 
 
 export default function Body() {
+
+    const [ clicked, setClicked ] = useState(false);
+
+    const  handleClick = () => {
+        setClicked(true)
+        setTimeout(() => setClicked(false), 300)
+    }
+
     return (
         <>
             <div className="body">
                 <div className='content'>
                     <h1>About me</h1>
                     <p>I'm an informational technology student specializing in .NET development. I enjoy programming, Linux, and DevOps. Experienced in databases, Web APIs, and MVC, I'm also learning React to expand my skills.</p>
-                    <button id='projects-btn'>Resume</button>
+                    <button
+                        className={`resume-btn ${clicked ? 'clicked' : ''}`}
+                        onClick={handleClick}
+                    >
+                        Resume
+                    </button>
                 </div>
                 <div className='bubble'>
-                    <img src="assets\profileimg.png" alt="" />
+                    <img src={ profileimg } alt="profileimg" />
                 </div>
             </div>
         </>
