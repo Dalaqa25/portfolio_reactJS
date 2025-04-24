0b2704a2bd7be0ce893032b53a4148f586bbaa74
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-04-24 14:33:24 +0400

    ds

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,12 @@
+import { useEffect } from 'react'
 import Header from '../components/header'
 import Body from '../components/body'
 import Projects from '../components/projects'
 import Waves from '../components/waves'
 import Form from "../components/form.jsx";
 import Skills from '../components/skills.jsx'
+// smoth scrolling
+import Lenis from '@studio-freight/lenis';
 //data
 import data from '../data.js'
 
@@ -18,6 +21,23 @@ function App() {
         )
     })
    
+    useEffect(() => {
+      const lenis = new Lenis({
+        smooth: true, // Enables smooth scrolling
+      });
+  
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+  
+      requestAnimationFrame(raf);
+  
+      return () => {
+        // Cleanup when component unmounts
+        lenis.destroy();
+      };
+    }, []);
 
   return (
     <>
