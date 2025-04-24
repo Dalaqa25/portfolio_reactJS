a328ad1919f4fe74cd921a59fae42a5721237c69
dalaqa25 <g.dalaqishvili01@gmail.com>
2025-04-24 10:43:17 +0400

    image optimization for frame

--- front/components/projects.jsx
+++ front/components/projects.jsx
@@ -5,26 +5,25 @@ export default function Projects(props) {
         <>
         <div id="projects" className="projects-container">
             <div className="projects">
-            <div className='img-container'>
-                <div className='header'>
-                    <div className='btn-container'>
-                        <div className='btn red-btn'></div>
-                        <div className='btn yellow-btn'></div>
-                        <div className='btn green-btn'></div>
+                <div className='img-container'>
+                    <div className='header'>
+                        <div className='btn-container'>
+                            <div className='btn red-btn'></div>
+                            <div className='btn yellow-btn'></div>
+                            <div className='btn green-btn'></div>
+                        </div>
+
+                        <div className='search-bar'>
+                            <p>🔒 {props.title}</p>
+                        </div>
                     </div>
 
-                    <div className='search-bar'>
-                        <p>🔒 {props.title}</p>
+                    <div className='imgframe'>
+                        <a target={"_blank"} href={props.link}>
+                            <img src={ props.img.src }  alt={props.img.alt} /> 
+                        </a>
                     </div>
                 </div>
-
-                <div className='imgframe'>
-                    <a target={"_blank"} href={props.link}>
-                     <img src={ props.img.src }  alt={props.img.alt} /> 
-                    </a>
-                </div>
-            </div>
-
             </div>
         </div>
         </>
